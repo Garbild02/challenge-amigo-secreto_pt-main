19e30599cddcfafe025fca3f21011fcb735aa7e8
Garbild02 <garbild0204@gmail.com>
2025-03-15 19:47:46 -0300

    Atualização
    Correção do erros de codigo que faziam os nomes adicionados não aparecer na lista

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 // Selecionando os elementos para (Campo de texto, Lista de nomes e o local onde sera mostrado o resultado)
-const inputAmigo = document.getElementById('amigo');
-const ListadeAmigos = document.getElementById('listaAmigos');
-const resultadoSorteio = document.getElementById('resultado');
+const inputAmigo = document.getElementById('amigo'); // Campo de texto para o nome
+const listaAmigos = document.getElementById('listaAmigos'); // Lista de nomes
+const resultado = document.getElementById('resultado'); // Local onde aparecerá o sorteado
 
-//Lista vazia para pegar os nomes inseridos
-const nomes = []
+// Criando uma lista vazia para guardar os nomes
+const nomes = [];
 
 //Função para adição dos nomes dos amigos
-function inserirAmigos(){
-    const nome = inputAmigo.ariaValueMax.trim();// Remove os espaços vazioa
+function adicionarAmigo() {
+    const nome = inputAmigo.value.trim(); //// Remove os espaços vazioa
 
-    //Verificação se o campo foi inserido
-    if (nome === ''){
-        alert(' Este campo não pode ser deixado em branco por favor, digite um nome');//Um alerta para informar o usuario que não deve deixar o campo em branco
-        return;
+    // //Verificação se o campo foi inserido
+    if (nome === '') {
+        alert('Por favor, digite um nome.'); //Um alerta para informar o usuario que não deve deixar o campo em branco
+        return; 
     }
 
-    //Inserir os nomes na lista
+    // Adiciona o nome à lista de nomes
     nomes.push(nome);
 
-    //Deixa a lista visivel na pagina
-    const itemLista =  document.createElement('li');
-    itemLista.textContent = nome;
-    ListadeAmigos.appendChild(itemLista);
-
-    //Limpando a caixa de texto
+    // Criação de lista, Verificação se o texto foi digitado e inserção do item na lista do html
+    const itemLista = document.createElement('li');
+    itemLista.textContent = nome; 
+    listaAmigos.appendChild(itemLista); 
+    
+    // Limpa o campo de texto e colocando o cursor novamente no campo
     inputAmigo.value = '';
     inputAmigo.focus();
 }
